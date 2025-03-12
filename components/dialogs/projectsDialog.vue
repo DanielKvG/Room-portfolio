b25e1444ab2c6ef6f9ddaca7d2405148bb9ea100
DanielKvG <d.kooyman19@gmail.com>
2025-03-12 12:58:59 +0100

    changed modal to slideover

--- components/dialogs/projectsDialog.vue
+++ components/dialogs/projectsDialog.vue
@@ -1,9 +1,9 @@
 <template>
-    <UModal v-model="store.projectsDialog">
-        <div class="w-48 h-48 bg-white rounded-lg">
+    <USlideover v-model="store.projectsDialog" side="bottom" :ui="{ wrapper: 'justify-center', base: 'w-full max-w-6xl', rounded: 'rounded-t-2xl', height: 'max-h-[624px]' }">
+        <div class="w-48 h-96 bg-white rounded-lg">
             hi
         </div>
-    </UModal>
+    </USlideover>
 </template>
 
 <script setup lang="ts">
