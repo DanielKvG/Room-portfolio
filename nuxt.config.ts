05f94a8a17a03fa25f006adaae68f0b1abcf20df
DanielKvG <d.kooyman19@gmail.com>
2025-03-13 15:01:41 +0100

    set standard color mode to light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  colorMode: {
+    preference: 'light'
+  },
   vite: {
     server: {
       allowedHosts: ['danielkvg.com', 'www.danielkvg.com'],
