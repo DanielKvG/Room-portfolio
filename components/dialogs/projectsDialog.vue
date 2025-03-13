0e38359010e7cd0e7d4f53dc9dba2d91cc4c0c92
DanielKvG <d.kooyman19@gmail.com>
2025-03-13 02:23:15 +0100

    configured projects for web

--- components/dialogs/projectsDialog.vue
+++ components/dialogs/projectsDialog.vue
@@ -1,15 +1,21 @@
 <template>
-    <USlideover v-model="store.projectsDialog" side="bottom" :ui="{ wrapper: 'justify-center', base: 'w-full max-w-6xl', rounded: 'rounded-t-2xl', height: '' }">
-        <div class="flex flex-col h-[624px] px-4 pt-2 gap-4">
+    <USlideover v-model="store.projectsDialog" side="bottom" :ui="{ wrapper: 'justify-center', base: 'w-full max-w-3xl overflow-hidden', rounded: 'rounded-t-2xl', height: '' }">
+        <div class="flex flex-col h-[624px] px-4 pt-2 gap-4 lg:flex-row lg:gap-0 lg:px-0 lg:pt-0">
             
-            <div class="flex items-center justify-between">
+            <div class="flex items-center justify-between lg:hidden">
                 <h1 class="text-2xl">Projects</h1>
                 <!-- TODO: add dropdown for project selection -->
                 <UButton variant="ghost" icon="i-heroicons-x-mark-20-solid" size="xl" @click="[ store.projectsDialog = false ]"></UButton>
             </div>
-            
-            <div class="flex flex-col gap-4 h-full overflow-y-scroll">
-                <div class="flex flex-col gap-4" v-for="project in projects" :key="project.name">
+            <div class="hidden lg:flex flex-col w-64 h-full bg-primary-50">
+                <h1 class="text-2xl text-primary-950 px-4 pt-4 pb-8">Projects</h1>
+                <div v-for="page in pages" class="p-6 w-full cursor-pointer hover:bg-primary-100" :class="active === page ? 'bg-white' : ''" @click="active = page" :key="page">
+                <h6 class="text-lg text-medium">{{ page }}</h6>
+                </div>
+            </div>
+            <!-- TODO: configure for web -->
+            <div class="flex flex-col gap-4  h-full overflow-y-scroll items-center lg:w-full lg:pt-6" v-if="active === 'All'">
+                <div class="flex flex-col gap-4 lg:w-[416px]" v-for="project in projects" :key="project.name">
                     <p class="text-primary-600 text-lg text-semibold m-0"> {{ project.name }} </p>
                     <img :src="'/images/' + project.image">
                     <div class="flex items-start content-start gap-y-[4px] gap-x-[6px] self-stretch flex-wrap"> 
@@ -20,12 +26,17 @@
                     <p class="text-gray-800">{{ project.text }}</p>
                 </div>
             </div>
+            <div v-else class="flex flex-col gap-4  h-full overflow-y-scroll items-center lg:w-full lg:pt-6"> 
+                <p>More projects will be added soon, together with filtering of the projects.</p>
+            </div>
         </div>
     </USlideover>
 </template>
 
 <script setup lang="ts">
 import { useStore } from '~/store/store';
+
+const store = useStore()
 
 const projects = [
     {
@@ -42,6 +53,11 @@ const projects = [
     }
 ]
 
-const store = useStore()
+const pages = [ 'All', 'CAD', 'Program', 'Create', 'Design']
+const active = ref('All')
+
+onMounted(() => {
+  active.value = pages[0]
+})
 
 </script>
