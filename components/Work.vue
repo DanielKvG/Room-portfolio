10000549b842ba278df121a85fcfa89bd83cd2cc
DanielKvG <d.kooyman19@gmail.com>
2025-09-13 14:03:19 +0200

    added coming soon text

--- components/Work.vue
+++ components/Work.vue
@@ -2,12 +2,13 @@
   <div class="flex flex-col justify-end h-0 transition-all duration-500 ease-in-out overflow-hidden" :class="{'h-full' : store.page.subject == 'about me'}">
     <div class="flex flex-row gap-2 h-screen px-4 py-8">
       <div class="flex flex-col gap-4"> 
-        <h1>Coming soon!</h1>
+        <h1>Work experience</h1>
         <h2>Figrs</h2>
         <h2>Extendas</h2>
         <h2>University of Twente</h2>
         <h2>Lidl</h2>
         <h2>Gasterij Terborgh</h2>
+        <p>more coming soon</p>
       </div>
     </div>
   </div>
