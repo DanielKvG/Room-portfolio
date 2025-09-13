568f16f0ac8df01c1076861c152762eddd007480
DanielKvG <d.kooyman19@gmail.com>
2025-09-13 13:57:10 +0200

    added companies

--- components/Work.vue
+++ components/Work.vue
@@ -3,6 +3,11 @@
     <div class="flex flex-row gap-2 h-screen px-4 py-8">
       <div class="flex flex-col gap-4"> 
         <h1>Coming soon!</h1>
+        <h2>Figrs</h2>
+        <h2>Extendas</h2>
+        <h2>University of Twente</h2>
+        <h2>Lidl</h2>
+        <h2>Gasterij Terborgh</h2>
       </div>
     </div>
   </div>
