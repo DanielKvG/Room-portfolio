c628c8e200cc854f531a767c7ab1e806944bdd3c
DanielKvG <d.kooyman19@gmail.com>
2026-05-05 20:33:41 +0200

    fixed rotational controls by copying orbit to mesh

--- components/homeScene.vue
+++ components/homeScene.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="dragfield" class="absolute left-0 right-0 top-0 bottom-0 flex flex-col justify-center pointer-events-auto">
         <div 
-            class="h-96 w-3/4 transition-all duration-800 ease-in-out" 
+            class="h-96 w-3/4 transition-all duration-800 ease-in-out touch-none" 
             :class="{'h-full w-full': !store.page.open}" 
             ref="container">
         </div>
@@ -9,18 +9,19 @@
 </template>
 
 <script lang="ts" setup>
-import { AmbientLight, PerspectiveCamera, Scene, WebGLRenderer, Clock, HemisphereLight, DirectionalLight, Object3D, Raycaster, Vector2, Mesh, MeshBasicMaterial, MeshPhongMaterial, Material, Color } from 'three';
+import { AmbientLight, PerspectiveCamera, Scene, WebGLRenderer, Clock, HemisphereLight, DirectionalLight, Object3D, Raycaster, Vector2, Mesh, MeshBasicMaterial, MeshPhongMaterial, Material, Color, Quaternion, Vector3, Euler } from 'three';
 import { useElementSize, useWindowSize } from '@vueuse/core';
 import { useStore } from '~/store/store';
 import { useProjectStore} from '~/store/projectStore';
-import { OrbitControls, GLTFLoader, type GLTF, CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/Addons.js';
+import { OrbitControls, GLTFLoader, type GLTF, CSS3DRenderer, CSS3DObject, TransformControls } from 'three/examples/jsm/Addons.js';
 import { gsap } from 'gsap';
 import { Draggable } from 'gsap/Draggable';
 
 let renderer: WebGLRenderer
-let controls: OrbitControls
+let orbit: OrbitControls
 let basicRoom: GLTF
 let camera: PerspectiveCamera
+let hiddenCamera: PerspectiveCamera
 let scene: Scene
 let container: Ref<HTMLCanvasElement | null>
 let clock: Clock
@@ -28,6 +29,7 @@ let camTarget: Object3D
 let down: boolean
 let matBuffer: Array<Material> = []
 let dragger: Draggable
+let transformControl: TransformControls
 type projectVis = {
     gltf: GLTF | undefined
     url: string
@@ -39,7 +41,7 @@ var lastY = 0;
 var meshX = 0;
 var meshY = 0;
 
-const raycaster = new Raycaster()
+//const raycaster = new Raycaster()
 const store = useStore()
 const projectsStore = useProjectStore()
 const loader = new GLTFLoader()
@@ -75,6 +77,10 @@ function init() {
     camera.lookAt(0, -1, 0)
     scene.add(camera)
 
+    hiddenCamera = camera.clone()
+
+
+
     //----- AmbientLight for lighting up all faces (switch or light/dark mode)
     const ambientLight = new AmbientLight(0xffffff, 0.6);
 
@@ -107,6 +113,32 @@ function init() {
     loadNext(nextProject.value)
 }
 
+//Add transform controls
+function setTransformControls() {
+    // orbit = new OrbitControls( camera, renderer.domElement );
+    // orbit.update();
+    // orbit.addEventListener( 'change', updateRender );
+
+    transformControl = new TransformControls( camera, renderer.domElement)
+    transformControl.addEventListener( 'change', updateRender );
+    transformControl.addEventListener( 'dragging-changed', function ( event ) {
+
+        //orbit.enabled = ! event.value;
+        console.log('dragging changed')
+
+    } );
+
+    transformControl.setMode("rotate");
+    transformControl.size = 4
+    transformControl.space = 'world'
+
+    transformControl.showX = true;
+    transformControl.showY = true;
+    transformControl.showZ = true;
+
+    scene.add(transformControl)
+}
+
 //load modal function
 async function loadFirst(model: projectVis) {
     [model.gltf] = await Promise.all([loader.loadAsync(model.url)])
@@ -117,15 +149,15 @@ async function loadFirst(model: projectVis) {
 async function loadNext(model: projectVis) {
     [model.gltf] = await Promise.all([loader.loadAsync(model.url)])
     model.gltf.scene.position.set(-15, -2, 0)
+    //transformControl.attach(model.gltf.scene)
     scene.add(model.gltf.scene)
 }
 
 function nextModel() {
-    if (nextProject.value.gltf && currentProject.value.gltf) {
-        gsap.to(nextProject.value.gltf.scene.position, {x: 15, y: -2, z: 0, onComplete: removeModel, onCompleteParams: [nextProject.value.gltf.scene], duration: 2, ease: "power2.inOut"})
-        gsap.to(currentProject.value.gltf.scene.position, {x: 0, y: -2, z: 0, duration: 2, ease: "power2.inOut"})
-        // nextProject.value.gltf!.scene.position.set(15, -2, 0)
-        // currentProject.value.gltf!.scene.position.set(0, -2, 0)
+    if (currentProject.value.gltf) {
+        gsap.to(currentProject.value.gltf!.scene.position, {x: 0, y: -2, z: 0, duration: 2, ease: "power2.inOut"})
+        orbit.reset()
+        gsap.to(nextProject.value.gltf!.scene.position, {x: 17, y: -2, z: 0, onComplete: removeModel, onCompleteParams: [nextProject.value.gltf!.scene], duration: 2, ease: "power2.inOut"})
     }
 }
 
@@ -133,6 +165,7 @@ function removeModel(model: Object3D) {
     scene.remove(model)
     loadNext(nextProject.value)
 }
+    
 
 //Update functions
 function updateRender() {
@@ -160,48 +193,47 @@ function setRenderer() {
 
 //Add orbit controls
 function setOrbitControls() {
+    // const dragfield = document.getElementById('dragfield')
+    // if (!dragfield) {
+    //     console.log('dragfield not defined')
+    //     return
+    // } 
+    // else {
+    //     //dragfield.addEventListener('mousemove', onHighlight)
+    //     renderer.domElement.addEventListener('pointermove', function(e) {
+    //         if (down) {
+    //             lastX = e.clientX
+    //             lastY = e.clientY
+    //             down = false
+    //             console.log('stuck')
+    //         } else if (currentProject.value.gltf) {
+    //             const model = currentProject.value.gltf.scene
+    //             const dX = lastX - e.clientX
+    //             const dY = lastY - e.clientY
+    //             const currentRot = model.rotation
+    //             lastX = e.clientX
+    //             lastY = e.clientY
+    //             model.rotateX(currentRot.x + (dX * scale))
+    //             model.rotateY(currentRot.y + (dY * scale))
+    //         }
+    //     })
+    //     dragfield.addEventListener('pointerup', function() {
+    //         down = false
+    //         console.log('up')
+    //     })
+        
+    //     dragfield.addEventListener('pointerdown', function() {
+    //         down = true
+    //         console.log('down')
+    //     })
+    // }
 
-    const dragfield = document.getElementById('dragfield')
-    if (!dragfield) {
-        console.log('dragfield not defined')
-        return
-    } 
-    else {
-        // dragfield.addEventListener('mousemove', onHighlight)
-        // dragfield.addEventListener('pointerup', onSelect)
-        dragfield.addEventListener('pointerdown', function() {
-            down = true
-        })
-        dragfield.addEventListener('pointermove', function(e) {
-            if (currentProject.value.gltf && down) {
-                var lastX = currentProject.value.gltf.scene.position.x;
-                var lastY = currentProject.value.gltf.scene.position.y;
+    orbit = new OrbitControls( hiddenCamera, renderer.domElement)
+    orbit.enableDamping = true
+    orbit.maxPolarAngle = Math.PI / 2;
+    
 
-                var dx = e.offsetX - lastX;
-                var dy = e.offsetY - lastY;
 
-                lastX = currentProject.value.gltf.scene.position.x;
-                lastY = currentProject.value.gltf.scene.position.y;
-
-                meshX += dx;
-                meshY += dy;
-                
-                currentProject.value.gltf.scene.rotation.x = meshY * scale;
-                currentProject.value.gltf.scene.rotation.y = meshX * scale;
-            }
-        })
-        dragfield.addEventListener('pointerup', function() {
-            down = false
-        })
-
-        // dragger = new Draggable(document.getElementById('dragfield'), {
-        //     onDrag: dragAction,
-        //     onThrowUpdate: dragAction,
-        //     trigger: renderer.domElement,
-        //     throwProps: true,
-        //     throwResistance: 10000
-        // })
-    }
 
     // controls = new OrbitControls( camera, dragfield)
     // controls.target.set(0, -1, 0)
@@ -215,65 +247,58 @@ function setOrbitControls() {
     // controls.autoRotate = true
 }
 
-function dragAction() {
-    if (currentProject.value.gltf) {
-        var x = currentProject.value.gltf.scene.position.x;
-        var y = currentProject.value.gltf.scene.position.y;
-
-        var dx = x - lastX;
-        var dy = y - lastY;
-
-        lastX = x;
-        lastY = y;
-
-        meshX += dx;
-        meshY += dy;
-        
-        currentProject.value.gltf.scene.rotation.x = meshY * scale;
-        currentProject.value.gltf.scene.rotation.y = meshX * scale;
-    }
-}
-
 //Add animations
 function animate() {
-    //controls.update()
+    orbit.update()
+
+    var polar = orbit.getPolarAngle() - Math.PI / 3
+    var azimuth = orbit.getAzimuthalAngle() * -1
+
+    var q = new Quaternion()
+    var axis = new Vector3(0, 1, 0)
+
+    q.setFromAxisAngle(axis, azimuth)
+    q.setFromEuler(new Euler(polar, azimuth, 0))
+
+    currentProject.value.gltf?.scene.quaternion.copy( q )
+
     renderer.render(scene, camera)
 }
 
-function checkIntersection(event: MouseEvent): Object3D | undefined {
-    const coords = new Vector2(
-        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-        - ((event.clientY / renderer.domElement.clientHeight) * 2 - 1)
-    )
+// function checkIntersection(event: MouseEvent): Object3D | undefined {
+//     const coords = new Vector2(
+//         (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+//         - ((event.clientY / renderer.domElement.clientHeight) * 2 - 1)
+//     )
 
-    raycaster.setFromCamera(coords, camera)
-    const intersections = raycaster.intersectObject(currentProject.value.gltf!.scene, true)
+//     raycaster.setFromCamera(coords, camera)
+//     const intersections = raycaster.intersectObject(currentProject.value.gltf!.scene, true)
 
-    if (intersections.length > 0) {
-        console.log(intersections[0])
-        return intersections[0].object.parent!
-    }
-}
+//     if (intersections.length > 0) {
+//         console.log(intersections[0])
+//         return intersections[0].object.parent!
+//     }
+// }
 
 //Object hover function
-function onHighlight(event: MouseEvent) {
-    down = true
-    const object = checkIntersection(event)
-    if (object === store.highlighted) return
-    else if (object && pages.includes(object.name)) store.highlighted = object
-    else store.highlighted = null
-}
+// function onHighlight(event: MouseEvent) {
+//     down = true
+//     const object = checkIntersection(event)
+//     if (object === store.highlighted) return
+//     else if (object && pages.includes(object.name)) store.highlighted = object
+//     else store.highlighted = null
+// }
 
 //Object click function
-function onSelect(event: MouseEvent) {
-    if (down === false) {
-        const object = checkIntersection(event)
-        if (object && pages.includes(object.name)) {
-            store.setSelected(object)
-        }
-        else store.closePage()
-    }
-}
+// function onSelect(event: MouseEvent) {
+//     if (down === false) {
+//         const object = checkIntersection(event)
+//         if (object && pages.includes(object.name)) {
+//             store.setSelected(object)
+//         }
+//         else store.closePage()
+//     }
+// }
 
 function highlight(child: Object3D) {
     const mesh = <Mesh>child
@@ -312,6 +337,7 @@ watch(currentProject, () => {
 onMounted(() => {
     setRenderer()
     setOrbitControls()
+    //setTransformControls()
     renderer.setAnimationLoop(animate)
 })
 
